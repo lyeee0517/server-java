0650d79d4036075460277fb1ed9528905ac5c47c
yelee <yelee@egemco.com>
2025-10-24 13:56:12 +0900

    fix : auditing 수정 - 입력자, 수정자 빼면서 하나의 파일로 변경

--- src/main/java/kr/hhplus/be/server/core/util/auditing/BaseEntity.java
+++ src/main/java/kr/hhplus/be/server/core/util/auditing/BaseEntity.java
@@ -1,29 +1,26 @@
 package kr.hhplus.be.server.core.util.auditing;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.EntityListeners;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Embeddable
-public class BaseEntity implements Serializable {
-
-    @CreatedBy
-    @Column(name = "RGTR_ID", updatable = false)
-    private String rgtrId; //등록자
+public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "REG_DT", updatable = false)
+    @Column(name = "REG_DT", nullable = false, updatable = false)
     private LocalDateTime regDt; //등록일시
+
+    @LastModifiedDate
+    @Column(name = "MODF_DT", nullable = false, insertable = false)
+    private LocalDateTime modfDt; //수정일
 
 }
